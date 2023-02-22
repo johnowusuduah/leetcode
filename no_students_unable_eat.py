49d309ca70203170ba1c3159c9034f9cd388e4f9
johnowusuduah <johnoduah@yahoo.co.uk>
2023-02-22 16:38:03 -0500

    no students unable to eat

--- no_students_unable_eat.py
+++ no_students_unable_eat.py
@@ -1,3 +1,4 @@
+# QUESTION
 # DESCRIPTION
 #The school cafeteria offers circular and square sandwiches at lunch break, referred to by numbers 0 and 1 respectively. All students stand in a queue. 
 #Each student either prefers square or circular sandwiches. The number of sandwiches in the cafeteria is equal to the number of students. The sandwiches 
