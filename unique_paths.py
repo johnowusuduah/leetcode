4bb35de293b8a37873cc823b0be708f74c03ef19
johnowusuduah <johnoduah@yahoo.co.uk>
2023-03-19 14:48:43 -0400

    unique path inplace

--- unique_paths.py
+++ unique_paths.py
@@ -6,19 +6,32 @@
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        rows, cols = m, n
-        prevRow = [0]*cols #initialize row of zeros to indicate out of bounds
+        # rows, cols = m, n
+        # prevRow = [0]*cols #initialize row of zeros to indicate out of bounds
 
-        for _ in range(rows): #initialize current as the last row with first loop and shift upwards
-            curRow = [0]*cols #same size as previous row
-            curRow[cols-1] = 1 #the last element of each row which is the same as the last column in
+        #TWO ARRAY DP SOLUTION
+        # for _ in range(rows): # initialize current as the last row with first loop and shift upwards
+        #     curRow = [0]*cols #same size as previous row
+        #     curRow[cols-1] = 1 #the last element of each row which is the same as the last column in
 
-            #loop backwards starting from the penultimate element in each curRow and update values in curRow
-            #we start from the penultimate element because we have already defined the last element as 1
-            for c in range(cols-2, -1, -1):
-                #each value of curRow is equal to the sum of the value to it's right and the value at the same
-                #index of the prevRow or the row below it
-                curRow[c] = curRow[c+1] + prevRow[c]
-            prevRow = curRow
+        #     #loop backwards starting from the penultimate element in each curRow and update values in curRow
+        #     #we start from the penultimate element because we have already defined the last element as 1
+        #     for c in range(cols-2, -1, -1):
+        #         #each value of curRow is equal to the sum of the value to it's right and the value at the same
+        #         #index of the prevRow or the row below it
+        #         curRow[c] = curRow[c+1] + prevRow[c]
+        #     prevRow = curRow
             
-        return prevRow[0]
+        # return prevRow[0]
+
+        #INPLACE DP SOLUTION
+        rows, cols = m, n
+        curRow = [0] * cols
+
+        for _ in range(rows):
+            curRow[cols-1] = 1
+
+            for c in range(cols-2, -1, -1):
+                curRow[c] = curRow[c + 1] + curRow[c]
+
+        return curRow[0]
